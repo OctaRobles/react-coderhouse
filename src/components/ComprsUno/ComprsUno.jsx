8d6642986e826215b5229e89f97e4c47f52314fa
= <=>
2023-07-13 01:05:12 -0300

    tercer commit

---
+++ src/components/ComprsUno/ComprsUno.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './EstilosComprsUno.css'
+const ComprsUno = (prop_greeting) => {
+    return(
+        <div className="titulo1">
+            <h1>Bienvenida a toda persona!!</h1>
+        </div>
+    )
+}
+
+export default ComprsUno;
